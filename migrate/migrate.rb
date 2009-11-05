06998dafd6b8560252363e52149fce7f3ba9db1e
Mark Meves <markmeves@mark-mevess-macbook-pro.local>
2009-11-05 09:54:51 -0500

    lots of stuff in StructDiff in conjuction with migrations

--- migrate/migrate.rb
+++ migrate/migrate.rb
@@ -2,49 +2,130 @@
 require File.dirname(__FILE__)+'/../cli'
 require 'hpricot'
 require 'csv'
+require 'builder'
 
 module Markus
   module Migrate
+    class MigrateException < Exception; end
     class Migrate
       include Cli::App
       def initialize
         cli_pre_init
         @cliDescription = "Pulls in data from excel sheets and spits it out to XML"
         @cliCommands = {
-          :transform => {
-            :description => 'With no arguments, generates a transformation file template '+
-              '(ruby code) and outputs it to stdout. If arguments are provided they should be '+
-              'the TRANSFORMATION_FILE and the CSV_FILE',
-            :arguments => {
-              :optional => [ 
-                {:name=>:TRANSFORMATION_FILE, :description=>"a ruby file with settings for how to process the csv file" },
-                {:name=>:CSV_FILE, :description=>"the file you want to process" }
-              ]
-            }
+          :generate_transform_scaffold => {
+            :description => 'Generates a transformation file template '+
+              '(ruby code) and outputs it to STDOUT.'
           },
-        }
+          :csv_to_xml => {
+            :description => 'the csv will be run through the filter and data will be added '+
+            'to an existing xml document (by calling user-defined methods.) Outputs result to STDOUT.',
+            :arguments => {
+              :required => [
+                { :name => :CSV_FILE, 
+                  :description => 'the csv we are processing', 
+                  :validations => [
+                    :file_must_exist
+                  ]
+                },
+                { :name => :STARTING_XML_FILE, 
+                  :description => 'the xml file to add to', 
+                  :validations => [
+                    :file_must_exist
+                  ]
+                }
+              ] #end required
+            } #end arguments
+          } #end command          
+        } #end commands
         cli_post_init
       end
     
       protected 
       
-      def cli_execute_transform
-        if @cliArguments.size == 0
-          return generate_template_file
-        else 
-          return transform
+      def build_xml
+        return Builder::XmlMarkup.new(:indent=>2, :margin=>4)
+      end
+
+      def csv_to_xml_start; 
+
+      end
+      
+      def log_change msg
+        @changeLog ||= File.open('./migrate-change-log', 'a+')        
+        @changeLog.puts(msg)
+        STDERR.puts(msg)
+      end
+      
+      def csv_to_xml_add_row; end
+      
+      def csv_to_xml_finish; end
+      
+      def cli_execute_csv_to_xml
+        csv_to_xml_start
+        @csvRows = CSV.read @cliArguments[:CSV_FILE]
+        if (@csvRows.size < 1)
+          raise MigrateException.new("unable to retrieve one or more rows from \"#{@cliArguments[:CSV_FILE]}\"") 
+        end
+        @csvRows.each_with_index do |row,index|     
+          matchingPatternData = nil
+          @withRowsMatchingPattern.each do |patternData|
+            if (
+              (patternData[:line_index] && index==patternData[:line_index]) ||
+              (patternData[:pattern]    && patternData[:pattern].match(row.join(',')))
+            )
+              matchingPatternData = patternData
+              break
+            end
+          end
+          doThis = matchingPatternData ? matchingPatternData[:action].to_s : 'default'
+          # STDERR.print "#{doThis}."
+          next if doThis == 'skip' # small optimization
+          if (@fieldNames)
+            useRow = {}
+            @fieldNames.each do |stringKey, numericIndex|
+              useRow[stringKey] = row[numericIndex]
+            end
+            # useRow.merge!(@extraFields)
+          else
+            useRow = row
+          end
+          __send__('csv_process_row_'+doThis,matchingPatternData,useRow,index,:orig_row => row)
+        end
+        csv_to_xml_finish
+      end
+      
+      # user will almost always want to override this.  this is to serve as a simple example
+      def csv_process_row_default(patternData, row, index, opts)
+        STDOUT << @xm.row(:index => index){
+          row.each do |key,value|
+            @xm.__send__(key,value) unless value.nil?
+          end
+        }
+      end      
+      
+      def csv_process_row_flatten_section_name_to_field_value(patternData, row, index, opts)
+        @extraFields ||= {}
+        @extraFields[patternData[:field_name]] = opts[:orig_row][0]
+      end
+      
+      def csv_process_row_is_field_names(patternData, row, index, xtra)
+        origRow = xtra[:orig_row]
+        @fieldNames = {}
+        origRow.each_with_index do |value,index|
+          next if value.nil?
+          @fieldNames[value.gsub(/[a-z0-9_]/, '').downcase] = index
         end
       end
       
-      def generate_template_file
+      def cli_execute_generate_transform_scaffold
         File.open(File.dirname(__FILE__)+'/data/transform_template.rb.tmpl','r') do |f|
           body = f.readlines.join('')
           body.gsub!('%%timestamp%%',Time.now.strftime("%a, %d %b %Y"));
           body.gsub!('%%app name%%',$PROGRAM_NAME);
           STDOUT.syswrite body
         end
-      end
-    
+      end #def      
     end #class Migrate
   end #module Migrate
 end #module Markus
