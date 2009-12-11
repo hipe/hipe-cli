172a8315b5fa57a184f11d413f67e4cd8a126242
Mark Meves <mark.meves@gmail.com>
2009-12-11 14:19:20 -0500

    about to try and speed up basics_spec.rb by only creating the app object once.  Note the actual reason we are doing this is not to speed up the tests but to see if the app can withstand several requests in its lifetime.

--- spec/basics_spec.rb
+++ spec/basics_spec.rb
@@ -1,11 +1,11 @@
 # bacon spec/basics_spec.rb 
+require 'hipe-core/structdiff'
 require File.dirname(__FILE__)+'/helpers/test-strap.rb'
 require File.dirname(__FILE__)+'/helpers/shared_one.rb'
 
 Oh = OrderedHash
 
 describe Hipe::Cli::App,"basics" do
-
   it "should reflect correctly on nonexistent commands (b1)" do
     @app = AppOne.new    
     @app.cli.commands[:not_there].should.equal nil
@@ -17,27 +17,30 @@ describe Hipe::Cli::App,"basics" do
    
   it "should parse required, optionals and splat correctly (b3)" do
     @app = AppOne.new
-    request = @app.cli.commands[:bark] << shell!('one two three four five six seven')
-    request.cli_tree.should == Hipe::Cli::Request[
+    request = @app.cli.commands[:bark].prepare_request shell!('one two three four five six seven')
+    have = request.cli_tree
+    want = Hipe::Cli::OrderedRequest[
       :options=>Oh[],
       :required=>Oh[:target,'one', :req2,'two'],
-      :optionals=>Oh[:optl1,'three',:optl2,'four'],
-      :splat=>{"splat"=>["five", "six", "seven"]},
+      :optionals=>Oh[:opt1,'three',:opt2,'four'],
+      :splat=>{:splat=>["five", "six", "seven"]},
       :extra=>Oh[]
     ]
+    diff = Hipe::StructDiff.diff(have, want)
+    diff.summarize.should == '(none)'
   end
 
   it "should parse extra arguments correctly with empty grammar (b4)" do
-    command = Hipe::Cli::Command.new(:whatever, {})
-    request = command << shell!('one two three')
-    request.cli_tree.should == Hipe::Cli::Request[
+    command = Hipe::Cli::Command.new(nil, :whatever, {})
+    request = command.prepare_request shell!('one two three')
+    request.cli_tree.should == Hipe::Cli::OrderedRequest[
       :options=>Oh[],:required=>Oh[],:optionals=>Oh[],:splat=>{}, 
       :extra=>{0=>'one',1=>'two',2=>'three'}
     ]
   end
   
   it "should parse options correctly (b5)" do
-    command = Hipe::Cli::Command.new(:whatever, {
+    command = Hipe::Cli::Command.new(nil,:whatever, {
       :options => {
         '-x --opt1' => {},
         '-o --opt2' => {:type=>:increment},
@@ -45,33 +48,34 @@ describe Hipe::Cli::App,"basics" do
         '-z --opt4' => {:type=>:boolean}
       }
     })
-    args = shell!('--opt1=loud -ooooo --opt4')
-    request = command << args
+    #args = shell!('--opt1=loud -ooooo --opt4')
+    args = ["--opt1=loud", "-ooooo", "--opt4"]
+    request = command.prepare_request args
   
-    request.cli_tree.should == Hipe::Cli::Request[
+    request.cli_tree.should == Hipe::Cli::OrderedRequest[
       :options=>Oh[:opt1,'loud', :opt2,5, :opt4,true],
       :required=>Oh[],:optionals=>Oh[],:splat=>{}, :extra=>Oh[]
     ]
   end
   
   it "should parse an empty string, possibly with errors (b6)" do
-
-      @app = AppOne.new
-    request = @app.cli.commands[:bark] << shell!('')
-    request.should.be.kind_of Hipe::Cli::Request
-    target = Hipe::Cli::Request[:options,Oh[], :required,Oh[], :optionals,Oh[], :splat,{}, :extra,Oh[]]
+    @app = AppOne.new
+    request = @app.cli.commands[:bark].prepare_request []
+    request.should.be.kind_of Hipe::Cli::OrderedRequest
+    target = Hipe::Cli::OrderedRequest[:options,Oh[], :required,Oh[], :optionals,Oh[], :splat,{}, :extra,Oh[]]
     request.cli_tree.should.equal target
   end
   
   it "should parse optionals (b7)" do
-    request = @app.cli.commands[:bark] << shell!('alpha beta gamma delta')
-    tree = request.cli_tree
-    tree.should.be.kind_of Hash
-    target = Hipe::Cli::Request[:options,Oh[], :required,Oh[
+    request = @app.cli.commands[:bark].prepare_request shell!('alpha beta gamma delta')
+    have = request.cli_tree
+    have.should.be.kind_of Hash
+    want = Hipe::Cli::OrderedRequest[:options,Oh[], :required,Oh[
         :target, 'alpha', :req2, 'beta'
       ], :optionals,Oh[
-      :optl1, 'gamma', :optl2, 'delta'
+      :opt1, 'gamma', :opt2, 'delta'
     ], :splat,{}, :extra,Oh[]]
-    tree.should.equal target
+    diff = Hipe::StructDiff.diff(want, have).summarize
+    diff.should == '(none)'
   end
 end
