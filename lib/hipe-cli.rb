ff8cbc0efdf2097d01dd9bd5a031e3468c7c8e23
Mark Meves <mark.meves@gmail.com>
2009-12-14 05:37:05 -0500

    about to erase range validator for now

--- lib/hipe-cli.rb
+++ lib/hipe-cli.rb
@@ -1,10 +1,15 @@
 require 'orderedhash'
 require 'ostruct'
+require 'rubygems'
+require 'ruby-debug'
+gem     'hipe-core', '0.0.2'
+require 'hipe-core/ascii-typesetting'
 require 'hipe-core/lingual'
+
 module Hipe
   module Cli
     VERSION = '0.0.4'
-    DIR = File.expand_path('../../',__FILE__) # for examples run from tests :/
+    DIR = File.expand_path('../../',__FILE__) # only used by tests that use examples :/
     AppClasses = {}
     def self.included klass
       cli = Cli.new(klass)
@@ -16,12 +21,11 @@ module Hipe
       AppClasses[klass.to_s] = klass
     end
     module AppClassMethods
-      def cli
-        @cli
-      end
+      def cli; @cli end
     end
     class Cli
       attr_reader :commands
+      attr_accessor :description
       def initialize(klass)
         @app_class = klass
         @commands = Commands.new
@@ -39,7 +43,50 @@ module Hipe
         @commands.add(name, *list, &block)
       end
       def run argv
-        @commands[argv.shift].run(argv)
+        if (cmd = @commands[name = argv.shift])
+          begin
+            cmd.run(argv)
+          rescue OptionParser::ParseError => e
+            e.reason
+          end
+        else
+          bad_command(name)
+        end
+      end
+      def bad_command(name)
+        return "done.\n" if name.nil? and @commands.size == 0;
+        list = Hipe::Lingual::List[@commands.map{|pair| %{"#{pair[1].full_name}"}}].or()
+        s = %{Unexpected command "#{name}".  Expecting #{list}.\n}
+        s << %{See "#{program_name} -h" for more info.\n} if @commands['-h']
+        s
+      end
+      def program_name; File.basename($0,'*.rb') end
+      def help(cmd_name=nil)
+        opts = OptionParser.new # hack just to use its display.  See Version 0.0.2 also
+        list = opts.instance_variable_get('@stack')[2]
+        commands_size = @commands.size
+        opts.banner = generate_banner + "\n\n" +
+          Hipe::Lingual.en{sp(np(adjp('available'),'subcommand',commands_size,:say_count=>false))}.say.capitalize + ':'
+        @commands.each do |key, command|
+          switch = OptionParser::Switch.new(nil,nil,nil,[command.full_name],nil,command.desc_arr,Proc.new{})
+          list.append(switch,[switch.short],[switch.long])
+        end
+        return opts.to_s
+      end
+      def screen; @screen ||= OpenStruct.new({:width=>77}); @screen end
+      def generate_banner
+        lines = []
+        lines <<  (%{#{program_name} - #{description}\n}) if description
+        left = %{usage: #{program_name} }
+        right = Hipe::AsciiTypesetting::FormattableString.new('')
+        right.replace( [@commands.select{|i,cmd| OptionLikeCommand === cmd}.map do |c|
+            '[' + ([c[1].short_name,c[1].long_name].compact * '|') + ']'
+          end * ' ', "COMMAND [OPTIONS] [ARG1 [ARG2 [...]]]"
+        ].compact * ' ')
+        right_column_width = [20, screen.width-left.length].max
+        lines << left + right.word_wrap_once!(right_column_width)
+        lines << right.word_wrap!(right_column_width).indent!(left.length) if right.size > 0
+        lines * "\n"
       end
     end
     class Commands < OrderedHash
@@ -52,7 +99,7 @@ module Hipe
       end
       def add(name, *list, &block)
         command = CommandFactory.command_factory(name, *list, &block)
-        name_str = command.name.to_s;
+        name_str = command.full_name;
         command.aliases.each do |aliaz|
           raise Exception.f(%{For now we can't redefine commands ("#{aliaz}")}) if @aliases[aliaz]
           @aliases[aliaz] = name_str
@@ -77,10 +124,10 @@ module Hipe
         end
       end
     end
-    module OptParseyCommandElement # commands, required arguments, optional arguments, options (switches) and splat
+    module OptParseyCommandElement #required arguments, optional arguments, options (switches) and splat
       attr_accessor :hipe_type, :app_instance
       attr_reader :description
-      def internal_name   # @TODO this will probably go now that we have integrated w/ optparse so much
+      def main_name   # @TODO this will probably go now that we have integrated w/ optparse so much
         str = nil
         if (@long && @long.size > 0)
           str = /^-?-?(.+)/.match(@long[0]).captures[0]
@@ -91,41 +138,35 @@ module Hipe
         end
         str.gsub('-','_').downcase.to_sym
       end
-      def natural_name
-        /^-?-?(.+)/.match(@long[0]).captures[0]
-      end
+       def surface_name
+         /^-?-?(.+)/.match(@long[0]).captures[0]
+       end
       LONG_NAME_WITHOUT_ARGS = /^--([-_a-z0-9]+)/i
       def self.parse_grammar(name,*list)
         o = OpenStruct.new()
         if (Symbol===name)
-          o.internal_name = name
+          o.main_name = name
         elsif (String===name)
           if md = LONG_NAME_WITHOUT_ARGS.match(name)
             o.long_name = md[1]
-            o.internal_name = o.long_name
+            o.main_name = o.long_name
           elsif md = /^-([a-z0-9])$/i.match(name)
             o.short_name = md[1]
             if list.size>0 and md = LONG_NAME_WITHOUT_ARGS.match(list[0])
               o.long_name = md[1]
             end
-            o.internal_name = (o.long_name || o.short_name).downcase.gsub('-','_').to_sym
+            o.main_name = (o.long_name || o.short_name).downcase.gsub('-','_').to_sym
           else
-            o.internal_name = name
+            o.main_name = name
           end
         else
-          raise Exception.f(%{bad type for name: #{name.class}},:type=>:grammar_grammar)
+          raise Exception.f(%{bad type for name: #{name.class}},:type=>:grammar_grammar) # leave type in for tests
         end
         if (idx = list.find_index{|x| String===x and /^[^-]/ =~ x  })
           o.description = list[idx]
         end
         o
       end
-      def take_names(o) # take the contents of a parse tree containing internal_name, long_name, etc
-        o.instance_variable_get('@table').each do |name,value|
-          instance_variable_set(%{@#{name}}, value)
-        end
-      end
-      def name; @internal_name end
     end
     module OptParseSwitch;
       include OptParseyCommandElement
@@ -172,14 +213,30 @@ module Hipe
         end
       end
     end
+    module CommandLike # probably no reason to be a module
+      attr_writer :app_instance
+      attr_reader :description
+      def take_names(o) # take the contents of a parse tree containing main_name, long_name, etc
+        o.instance_variable_get('@table').each do |name,value|   # see OptParseyCommandElement#parse_grammar
+          instance_variable_set(%{@#{name}}, value)
+        end
+      end
+      def main_name; @main_name end
+      def full_name; main_name.to_s end # always return a string here! optparse wants it for length()
+      def as_method_name; main_name.to_s.gsub('-','_').downcase.to_sym end
+    end
     class Command
-      include OptParseyCommandElement
+      include CommandLike
       def initialize(names, &block)
         @block = block
         take_names(names)
       end
       def aliases
-        [@internal_name.to_s]
+        [@main_name.to_s]
+      end
+      def desc_arr  # if we wanna be like optparse, return an array.
+        return [] unless @description
+        Hipe::AsciiTypesetting::FormattableString.new(@description).word_wrap!(39).split("\n") # @todo
       end
       def run(argv)
         return run_with_application(argv) unless @block #if there is no definition block we pass the args raw
@@ -191,14 +248,14 @@ module Hipe
         }
         opts = OptionParser.new do |opts| # build all the switches for options, required, optionals, splat in one go
           @option_parser = opts           # is made available thru opts() so definitions can e.g. opts.separator()
-          @definitions = []     # gets populated in the next line
+          @definitions = []               # gets populated in the next line
           self.instance_eval(&@block)     # runs all the option(), required(), optional(), splat() definitions
-          @definitions.each do |my_info| # with each one of those definition we 'stored' ...
+          @definitions.each do |my_info|  # with each one of those definition we 'recoreded' ...
             first_arg = my_info.first_arg
             unless /^-/ =~ first_arg
-              first_arg = %{--#{first_arg} VALUE} # positional arguments use names without '-'
-            end
-            switch = opts.define(first_arg,*my_info.list,&my_info.block)
+              first_arg = %{--#{first_arg} VALUE} # positional arguments will need proper names and parameters
+            end                           # only for their construction (hack alert!)
+            switch = opts.define(first_arg,*my_info.list,&my_info.block) # make an optional, required, option, etc.
             switch.extend my_info.hipe_type
             switch.hipe_type = my_info.hipe_type
             switches[switch.hipe_type] << switch
@@ -210,9 +267,9 @@ module Hipe
             new_block = Proc.new() do |x|
               if (orig_block)
                 result_from_original = orig_block.call(x)
-                opt_values[switch.internal_name] = result_from_original
+                opt_values[switch.main_name] = result_from_original
               else
-                opt_values[switch.internal_name] = x
+                opt_values[switch.main_name] = x
               end
             end
             switch.instance_variable_set('@block',new_block)
@@ -234,7 +291,7 @@ module Hipe
 
         # complain about any required arguments that are not in the opt_values
         provided = opt_values.keys
-        missing = switches[RequiredPositional].select{|x| ! provided.include? x.internal_name}
+        missing = switches[RequiredPositional].select{|x| ! provided.include? x.main_name}
         error_missing(missing) if missing.size > 0
 
         # complain if there are any remaining unparsed arguments  #@TODO splat
@@ -246,7 +303,7 @@ module Hipe
       def flatten_args(switches, opt_values)
         arg_array = []
         switches[PositionalArgument].each do |switch|
-          arg_array << opt_values.delete(switch.internal_name) # ok if nil
+          arg_array << opt_values.delete(switch.main_name) # ok if nil
         end
         if (switches[OptParseSwitch].size > 0)
           arg_array << opt_values # even if it is empty
@@ -264,7 +321,7 @@ module Hipe
       end
       def error_missing(missing)
         e = OptionParser::MissingArgument.new
-        names = missing.map{|x| x.natural_name }
+        names = missing.map{|x| x.surface_name }
         sp = Hipe::Lingual.en{ sp(np(adjp('missing','required'),'argument',names))  }
         e.reason = sp.say
         raise e
@@ -315,30 +372,37 @@ module Hipe
         @definitions << OpenStruct.new(definition)
       end
       def run_with_application(argv)
-        @app_instance.send(name.to_s.gsub('-','_'), *argv)
+        if @app_instance.respond_to?(as_method_name)
+          @app_instance.send(as_method_name, *argv)
+        elsif([:help].include? as_method_name)
+          @app_instance.cli.help(*argv)
+        else
+          raise Exception.f(%{Please implement "#{as_method_name}"})
+        end
       end
     end
     class OptionLikeCommand < Command
-      include OptParseyCommandElement, OptParseyCommandElement
       def initialize(o, &block)
         take_names(o)
         @block = block
       end
       def aliases
-        [@internal_name.to_s, @short_name ? %{-#{@short_name}} : nil,@long_name ? %{--#{@long_name}} : nil].compact
+        [@main_name.to_s, short_name, long_name].compact
       end
+      def short_name; @short_name ? %{-#{@short_name}} : nil end
+      def long_name; @long_name ? %{--#{@long_name}} : nil end
     end
-    class Range < ::Range  # experiments with our first validation struct
-      def initialize(start,endo,exclusive=false)
-        super start,endo,exclusive
-      end
-      def self.[](start,endo)
-        return self.new(start,endo)
-      end
-      def match(value)
-        return self === value
-      end
-    end
+   # class Range < ::Range  # experiments with our first validation struct @fixme
+   #   def initialize(start,endo,exclusive=false)
+   #     super start,endo,exclusive
+   #   end
+   #   def self.[](start,endo)
+   #     return self.new(start,endo)
+   #   end
+   #   def match(value)
+   #     return self === value
+   #   end
+   # end
     class Exception < ::Exception
       attr_reader :details
       protected
@@ -346,7 +410,8 @@ module Hipe
         super(msg)
         @details = details
       end
-      def self.f(msg,details={}) # factory
+      def self.f(msg,details={}) # factory lets us be lazy about creating new classes
+        klass = nil
         if (details[:type])
           class_name = details[:type].to_s.gsub(/(?:^|_)([a-z])/){|x| $1.upcase}
           [class_name+'Exception', class_name].each do |klass_name|
